85c38b1cba3ee6d9ccfa254d5b96f705b14bd655
William Cannon <william.cannon@gmail.com>
2022-01-06 09:32:13 -0600

    skeleton updated - need Jeff to update parsing

--- score_keeper.py
+++ score_keeper.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import re
+import sys
 import json
-import glob
+import html_generator
+import s3_push
 
 
 # TODO:  Use some sort of cli argument passing library to support  (docopt / argparse and etc)
@@ -9,6 +11,7 @@ import glob
 # -h html file (true/false)
 # -s3 bucket name to push to (optional name)
 DB_FILE = "sk_db.json"
+
 
 def load_db(filename=DB_FILE):
     data = {}
@@ -27,6 +30,47 @@ def save_db(db_dict):
     return
 
 
+def parse_log(filename="operator.log"):
+    '''
+    Parse a log file, and return a dictionary of all info
+    key = datetime stamp
+    value = dictionary of our info
+    '''
+    log_dict = {}
+    with open(filename, 'r') as myfile:
+        for line in myfile:
+            # TODO FOR JEFF - grep out our info here (done in previous commit)
+            line_dict = {"dt": dt,
+                         "ip": ip,
+                         "aleo_addr": aleo_addr,
+                         "block": block}
+            log_dict[dt] = line_dict
+    return log_dict
+
+
+def main(filename=None):
+    db_dict = load_db()
+    log_dict = parse_log(filename)
+
+    # Update db_dict if value not already in it
+    for key in log_dict.keys():
+        if key not in db_dict.keys():  # meaning it is new
+            db_dict[key] = log_dict[key]
+    save_db(db_dict)  # so that db can be used later
+
+    # Create html file
+    html_generator.main()
+    s3_push.main()  # requires credentials on the server already e.g. ~/.aws/credentials file
+
+
+if __name__ == "__main__":
+    '''This allows us to add an already used log to our database, or to use the current one by default'''
+    filename = "operator.log"
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    main(filename)
+
+'''
 def main():
     count_dict = {}
     for fn in sorted(glob.glob("operator.log*")):
@@ -34,15 +78,18 @@ def main():
         with open(fn, 'r') as f:
             for line in f:
                 if re.search(pattern, line):
-                    #result returns list with datetime, ip, miner address, block
-                    result = re.findall(r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z).*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4}).*?\((aleo.*?)\) for block (\d+)", line)
-                    #returns just datetimes
-                    dt = re.findall(r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z)", line)
-                    #returns just ips
-                    ip = re.findall(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4})", line)
-                    #returns just miner addresses
+                    # result returns list with datetime, ip, miner address, block
+                    result = re.findall(
+                        r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z).*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4}).*?\((aleo.*?)\) for block (\d+)", line)
+                    # returns just datetimes
+                    dt = re.findall(
+                        r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z)", line)
+                    # returns just ips
+                    ip = re.findall(
+                        r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4})", line)
+                    # returns just miner addresses
                     aleo_addr = re.findall(r"\((aleo.*?)\)", line)
-                    #returns just block
+                    # returns just block
                     block = re.findall(r"for block (\d+)", line)
                     #l_dict = {'dt':dt, 'ip':ip, 'aleo_addr':aleo_addr, 'block':block}
                     if len(result) > 0:
@@ -51,14 +98,9 @@ def main():
                             count_dict[res] = count_dict[res] + 1
                         else:
                             count_dict[res] = 1
-                            
+
     for k, v in count_dict.items():
-        #print(k,v)
+        # print(k,v)
         with open("sk_db.json", "w") as f:
             json.dump(list(count_dict), f)
-          
-                        
-
-
-if __name__ == "__main__":
-    main()
+'''
