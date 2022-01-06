27a9364f2cde70dd2f91b74f164d64499b9d26d2
William Cannon <william.cannon@gmail.com>
2022-01-05 19:38:00 -0600

    html generation works - including example output html file

--- html_generator.py
+++ html_generator.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
+import sys
 import score_keeper
 import jinja2
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def create_ip_based_list(db_dict):
-    pass
-
-
-def create_aleo_addr_based_list(db_dict):
-    pass
-
-
-def main():
-    # load db into dictionary
-    db = score_keeper.load_db()
+def create_ip_based_list(db):
     # create ip descending list of block counts
     count_dict = {}  # {'x.x.x.x' : {'count': NUM, 'aleo_addr': 'aleo1XYZZZZ' }  }
     score_list = []
@@ -31,13 +23,39 @@ def main():
             count_dict[ip] = curr_count_item
     # create aleo addr descending list of block counts
     for k, v in count_dict.items():
-        score_list.append((k, v['count'], v['aleo_addr']))
+        # removing port num for display purposes
+        aleo_addr_ip = k.split(":")[0]
+        score_list.append((aleo_addr_ip, v['count'], v['aleo_addr']))
     sorted_scores = sorted(score_list, key=lambda ip: ip[1], reverse=True)
-    for item in sorted_scores:
-        print(item)
+    return sorted_scores
+
+
+def create_aleo_addr_based_list(db_dict):
+    pass
+
+
+def main():
+    # load db into dictionary
+    db = score_keeper.load_db()
+    ip_sorted_list = create_ip_based_list(db)
+    # for item in ip_sorted_list:
+    #    print(item)
 
     # call template fn to generate html
-    # save html file
+    env = Environment(
+        # loader=PackageLoader("yourapp"),
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape()
+    )
+    template = env.get_template("base.html")
+    html_file = template.render(vars=ip_sorted_list)
+    try:
+        outfile = open("leaderboard.html", "w")
+        outfile.write(html_file)
+        outfile.close()
+    except Exception as e:
+        print(f"Unable to save html file, {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
